653c2ff07a6ef4eb076808a59e611fd3ab32c4fc
Wendy <alteredco@gmail.com>
2020-07-04 09:51:21 +0100

    add reset styles

---
+++ src/Payment.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaidImage from './assets/universal-ebike-kit-1.jpg';
+
+function Payment() {
+  return(
+    <header className="header--payment">
+      <p>Thank you for your purchase.</p>
+    </header>
+  );
+}
