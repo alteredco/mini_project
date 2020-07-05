9d0daa5f6284fd3bef8f83426c31b572d0de9ff1
Wendy <alteredco@gmail.com>
2020-07-05 12:50:41 +0100

    add accordion form to landing

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, {useState, useRef } from 'react';
 import './Accordion.scss';
 import Chevron from  '../Chevron/Chevron';
 import Form from '../Form/Form';
 
 const Accordion = (props) => {
+
+  const [active, setActive] = useState("");
+
+  const[ height, setHeight] = useState("0px");
+
+  const content = useRef(null);
+
+  function toggleActive() {
+    setActive(active === ""? "active": "");
+    setHeight(active === "active" ?  "0px": `${content.current.scrollHeight}px`)
+  }
+
   return (
     <div className="accordion__container">
-      <button className="accordion__Btn">
+      <button className={`accordion__btn ${active}`} onClick={toggleActive}>
         <p className="accordion__title">
           {props.title}
         </p>
         <Chevron width={10} fill={"#fefefe"} />
       </button>
-      <div className="accordion__content">
+      <div ref={content} style={{maxHeight: `${height}`}} className="accordion__content">
         <Form />
         </div>
         </div>
