1fdeb290dc215bac883d539e958c8adc4fd33601
Wendy <alteredco@gmail.com>
2020-07-05 23:05:44 +0100

    update headline

--- src/components/Headline/Headline.js
+++ src/components/Headline/Headline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Headline.scss';
 
-const Headline =() =>{
+const Headline =(props) =>{
   return (
     <div className="headline__container">
     <h1 className="headline__title">Universal eBike Conversion Kit</h1>
